2c60150776ad01996a8ab7b3b68450f2e2a1fc80
fuhad479 <fuhadhasan479@gmail.com>
2023-08-10 19:10:40 +0600

    fixed slider issue

--- components/HomeOne/MainBannerCopy.js
+++ components/HomeOne/MainBannerCopy.js
@@ -1,47 +1,74 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation, EffectFade } from "swiper";
 
-const MainBanner = () => {
+const MainBanner = ({ headerRef }) => {
   const [isOpen, setOpen] = useState(false);
+  const [heroGap, setHeroGap] = useState()
 
+  useEffect(() => {
+    setHeroGap(headerRef.current?.offsetHeight)
+  }, [headerRef.current])
+
+  console.log(headerRef)
+  
   return (
-    <Swiper
-      modules={[Autoplay, Navigation, EffectFade]}
-      effect="fade"
-      autoplay
-      loop
-      slidesPerView={1}
-      navigation
-    >
-      <SwiperSlide>
-        <div
-          className="slider-inner"
-          style={{
-            background:
-              "url('/img/slideshow-one.jpg') no-repeat center / cover",
-          }}
-        ></div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div
-          className="slider-inner"
-          style={{
-            background:
-              "url('/img/slideshow-two.jpeg') no-repeat center / cover",
-          }}
-        ></div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div
-          className="slider-inner"
-          style={{
-            background:
-              "url('/img/slideshow-three.jpg') no-repeat center / cover",
-          }}
-        ></div>
-      </SwiperSlide>
-    </Swiper>
+    <div className="" style={{paddingTop: `${heroGap}px`}}>
+      <Swiper
+        modules={[Autoplay, Navigation, EffectFade]}
+        effect="fade"
+        autoplay
+        loop
+        slidesPerView={1}
+        navigation
+      >
+        <SwiperSlide>
+          <div
+            className="slider-inner"
+            style={{
+              background:
+                "url('/img/slideshow-one.jpg') no-repeat top / cover",
+            }}
+          ></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="slider-inner"
+            style={{
+              background:
+                "url('/img/slideshow-two.jpeg') no-repeat top / cover",
+            }}
+          ></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="slider-inner"
+            style={{
+              background:
+                "url('/img/slideshow-three.jpg') no-repeat top / cover",
+            }}
+          ></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="slider-inner"
+            style={{
+              background:
+                "url('/img/slideshow-four.jpeg') no-repeat top / cover",
+            }}
+          ></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="slider-inner"
+            style={{
+              background:
+                "url('/img/slideshow-five.jpg') no-repeat top / cover",
+            }}
+          ></div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
 };
 
